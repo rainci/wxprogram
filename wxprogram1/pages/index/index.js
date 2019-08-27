5c9e5b47d091e6d15cd7f3848cbdc5ea226ae8fd
rainci <2805242676@qq.com>
2019-08-27 16:32:59 +0800

    add page

--- wxprogram1/pages/index/index.js
+++ wxprogram1/pages/index/index.js
@@ -4,10 +4,29 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: 'Hello program',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    list: [
+      {
+        id: 'view',
+        name: '视图容器',
+        open:false,
+        pages: ['view', 'scroll-view', 'swiper', 'movable-view', 'cover-view']
+      },
+      {
+        id: 'content',
+        name: '基础内容',
+        open: false,
+        pages: ['text', 'icon', 'progress', 'rich-text']
+      }, {
+        id: 'form',
+        name: '表单组件',
+        open: false,
+        pages: ['button', 'checkbox', 'form', 'input', 'label', 'picker', 'picker-view', 'radio', 'slider', 'switch', 'textarea', 'editor']
+      }
+    ]
   },
   //事件处理函数
   bindViewTap: function() {
@@ -50,5 +69,20 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+  kindToggle(e) {
+    const id = e.currentTarget.id
+    const list = this.data.list
+    for (let i = 0, len = list.length; i < len; ++i) {
+      if (list[i].id === id) {
+        list[i].open = !list[i].open
+      } else {
+        list[i].open = false
+      }
+    }
+    this.setData({
+      list
+    })
+    wx.reportAnalytics('click_view_programmatically', {})
   }
 })
